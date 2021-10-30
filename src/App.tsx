689d923dd8b7aa460843ff18b6732591a3803eb2
Kirk Shillingford <tkshillinz@icloud.com>
2021-10-30 19:38:03 -0400

    more comments

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ const initialGame = () => ({ game: new Game() });
 export default function App() {
   const [state, setState] = useState(initialGame());
 
+  // this is where we update the state of our application
   const update = (value: number | "Restart") => {
     if (value !== "Restart") {
       state.game.setCell(value);
@@ -14,18 +15,21 @@ export default function App() {
     } else setState(initialGame());
   };
 
+  // our tiny little cell component
   const Cell = (num: number) => (
     <button key={num} id={`cell${num}`} onClick={() => update(num)}>
       {state.game.getCell(num) ?? ""}
     </button>
   );
 
+  // I really dislike curly braces
   const statusMessage = () => {
     if (state.game.winner) return `${state.game.winner} won the game!`;
     else if (state.game.isFull) return "The game is a draw!";
     else return `${state.game.turn}'s turn to play!`;
   };
 
+  // Putting it all together
   return (
     <div className="App">
       <h1>ReacTacToe</h1>
