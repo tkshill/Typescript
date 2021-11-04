76e7d5c06859c99e3e2ed7793e95b83f363d3f39
Kirk Shillingford <tkshillinz@icloud.com>
2021-11-04 15:38:26 -0400

    from spread to toArray()

--- src/GameClass.ts
+++ src/GameClass.ts
@@ -6,7 +6,7 @@ const keys = [2, 7, 6, 9, 5, 1, 4, 3, 8];
 
 // get every unique combination of 3 numbers and only keep the ones that sum to 15
 // I'll explain why this works later.
-const winningCombos = [...new Combination(keys, 3)].filter(
+const winningCombos = new Combination(keys, 3).toArray().filter(
   (nums) => nums.reduce((acc, num) => acc + num) === 15
 );
 
